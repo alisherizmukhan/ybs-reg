4675c7ad9319eb29e5651b149c6db04840a88e83
Alisher Izmukhan <alisher.izmukhan@nu.edu.kz>
2025-01-14 23:57:57 +0500

    ybs baby v1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import myImage from './sample.jpg';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to YBS Reg!</h1>
+      <img src={myImage} />
+    </div>
+  );
+}
+
+export default App;
